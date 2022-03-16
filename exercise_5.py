ce0e04ff92981a50c633c5f96dd421a3e4366b4a
charlieorch <charlieorch1@gmail.com>
2022-03-16 14:09:57 -0500

    minor change

--- exercise_5.py
+++ exercise_5.py
@@ -1,8 +1,10 @@
+
+
 class OutOfRangeException(Exception): pass
 
 def user_input():
     while True:
-        choice = input("Would you like to enter an integer number or a float number(i for integer/f for float)? ").lower()
+        choice = input("Would you like to enter an integer number ocler a float number(i for integer/f for float)? ").lower()
         if(choice == "i" or choice == "f"):
             if choice == "i":
                 while True:
@@ -15,7 +17,7 @@ def user_input():
                         print("Number must be an integer")
                     except OutOfRangeException:
                         print("Number(integer) must be greater than 0 or less than 500")
-            elif choice == "f":
+            if choice == "f":
                 while True:
                     try:
                         number = float(input("Please enter a number(float): "))
